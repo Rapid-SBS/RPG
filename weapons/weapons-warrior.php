a7484d21314d4770400f34389fbe06287ee999c6
Rapid-SBS <info@rapid-sbs.com>
2019-05-29 14:53:19 -0700

    Fix

--- weapons/weapons-warrior.php
+++ weapons/weapons-warrior.php
@@ -3,15 +3,16 @@
   <div class="accordion" id="accordion-swordshield">
       <div class="card-header text-center">
         <h4>Sword & Shield</h4>
-      </div>
+      </div><!-- /.card-header --> 
       <div class="card-header pad-5 text-center">
         <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
           <h6>Sword Strike</h6>
         </button>
-      </div>
+      </div><!-- /.card-header --> 
       <div id="collapseOne" class="collapse hidden" aria-labelledby="headingOne" data-parent="#accordion-swordshield">
         <div class="card-body light-transparency pad-10 text-center">
           <p class="card-text text-center">Grants bonus DEF during combat. <br>DMG = Weapon ATT + Player ATT. </p>
+        </div>
         <ul class="list-group text-center">
           <li class="list-group-item align-items-center light-transparency bg-orange" style="padding: .35rem 1.25rem;">
             <span class="badge badge-light text-orange">2</span>
@@ -25,7 +26,6 @@
           </li>
         </ul>
       </div>
-    </div>
       <div class="card-header pad-5 text-center">
         <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
           <h6>Shield Ally <span class="badge badge-light text-teal">2</span></h6>
