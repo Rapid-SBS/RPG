e02fd58db35a23b2261c8cf310b1237c38a08741
Rapid-SBS <info@rapid-sbs.com>
2019-05-29 14:32:36 -0700

    dfd

--- weapons-warrior.php
+++ weapons-warrior.php
@@ -9,169 +9,28 @@
 
   <title>RPGenerator</title>
 
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="css/spacing.css">
   <link rel="stylesheet" type="text/css" href="css/colors.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 
 </head>
 
 <body>
-<div class="container pad-10">
 
-  <!-- Sword & Shield -->
-  <div class="row justify-content-center">
-  <div class="col-4">
-  <div class="card text-white bg-orange mb-2" style="max-width: 18rem;">
-  	<div class="accordion" id="accordion-swordshield">
-      <div class="card-header text-center">
-        <h4>Sword & Shield</h4>
-      </div>
-      <div class="card-header pad-5 text-center">
-      	<button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          <h6>Sword Strike<h6>
-     	 </button>
-	  </div>
-      <div id="collapseOne" class="collapse hidden" aria-labelledby="headingOne" data-parent="#accordion-swordshield">
-       <div class="card-body text-center">
-              <p class="card-text text-center">Grants bonus DEF during combat. <br>DMG = Weapon ATT + Player ATT. </p>
-            </div>
-            <ul class="list-group text-center">
-              <li class="list-group-item align-items-center bg-orange" style="padding: .35rem 1.25rem;">
-                <span class="badge badge-light text-orange">2</span>
-                Weapon ATT
-              </li>
-            </ul>
-            <ul class="list-group text-center">
-              <li class="list-group-item align-items-center bg-orange" style="padding: .35rem 1.25rem;">
-                <span class="badge badge-light text-green">+2</span>
-                Bonus DEF
-              </li>
-            </ul>
-      </div>
-      <div class="card-header pad-5 text-center">
-	      <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-	          <h6>Shield Ally <span class="badge badge-light text-teal">1</span></h6>
-	      </button>
-	  </div>
-      <div id="collapseTwo" class="collapse hidden" aria-labelledby="headingTwo" data-parent="#accordion-swordshield">
-       <div class="card-body text-center">
-              <p class="card-text text-center">Use your shield to protect <br>yourself or one ally. <br>Grants +4 DEF for one turn. <br>Costs 1 Mana.</p>
-            </div>
-            <ul class="list-group text-center">
-              <li class="list-group-item align-items-center bg-orange" style="padding: .35rem 1.25rem;">
-                <span class="badge badge-light text-green">+4</span>
-                Ally DEF
-              </li>
-            </ul>
-      </div>
-  </div>   
-  </div><!-- /.card -->    
-  </div><!-- /.accordion -->
-  </div><!-- /.row -->
+<div class="y-flex-container pad-10">
 
-  <!-- Halberd-->
-  <div class="row justify-content-center">
-  <div class="col-4">
-  <div class="card text-white bg-orange mb-2" style="max-width: 18rem;">
-  	<div class="accordion" id="accordion-halberd">
-      <div class="card-header text-center">
-        <h4>Halberd</h4>
-      </div>
-      <div class="card-header pad-5 text-center">
-      	<button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
-          <h6>Jab<h6>
-     	 </button>
-	  </div>
-      <div id="collapseThree" class="collapse hidden" aria-labelledby="headingThree" data-parent="#accordion-halberd">
-       <div class="card-body text-center">
-              <p class="card-text text-center">Thrust your Halberd at an enemy. <br>DMG = Weapon ATT + Player ATT. </p>
-            </div>
-            <ul class="list-group text-center">
-              <li class="list-group-item align-items-center bg-orange" style="padding: .35rem 1.25rem;">
-                <span class="badge badge-light text-orange">3</span>
-                Weapon ATT
-              </li>
-            </ul>
-      </div>
-      <div class="card-header pad-5 text-center">
-	      <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
-	          <h6>Sweeping Blow <span class="badge badge-light text-teal">3</span></h6>
-	      </button>
-	  </div>
-      <div id="collapseFour" class="collapse hidden" aria-labelledby="headingFour" data-parent="#accordion-halberd">
-       	<div class="card-body text-center">
-              <p class="card-text text-center">Attack all enemies. <br>DMG = Weapon ATT + Player ATT. <br>Costs 3 Mana.</p>
-            </div>
-            <ul class="list-group text-center">
-              <li class="list-group-item align-items-center bg-orange" style="padding: .35rem 1.25rem;">
-                <span class="badge badge-light text-orange">3</span>
-                Weapon ATT
-              </li>
-            </ul>
+<?php include 'weapons/weapons-warrior.php'; ?>
 
-      </div>
-  </div>   
-  </div><!-- /.card -->    
-  </div><!-- /.accordion -->
-  </div><!-- /.row -->
+<?php include 'weapons/archer-crossbow.php'; ?>
 
-  <!-- Battleaxe-->
-  <div class="row justify-content-center">
-  <div class="col-4">
-  <div class="card text-white bg-orange mb-2" style="max-width: 18rem;">
-  	<div class="accordion" id="accordion-battleaxe">
-      <div class="card-header text-center">
-        <h4>Battleaxe</h4>
-      </div>
-      <div class="card-header pad-5 text-center">
-      	<button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
-          <h6>Cleave<h6>
-     	 </button>
-	  	</div>
-      <div id="collapseFive" class="collapse hidden" aria-labelledby="headingFive" data-parent="#accordion-battleaxe">
-      <div class="card-body text-center">
-              <p class="card-text text-center">Swing your mighty Battleaxe. <br>DMG = Weapon ATT + Player ATT.</p>
-      </div>
-            <ul class="list-group text-center">
-              <li class="list-group-item align-items-center bg-orange" style="padding: .35rem 1.25rem;">
-                <span class="badge badge-light text-orange">2</span>
-                Weapon ATT
-              </li>
-            </ul>
-      </div>
-      <div class="card-header pad-5 text-center">
-	      <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="true" aria-controls="collapseSix">
-	          <h6>Sunder Armor <span class="badge badge-light text-teal">3</span></h6>
-	      </button>
-	  </div>
-      <div id="collapseSix" class="collapse hidden" aria-labelledby="headingSix" data-parent="#accordion-battleaxe">
-       <div class="card-body text-center">
-              <p class="card-text text-center">Pierce enemy defenses. <br>DMG = Weapon ATT + Player ATT.<br>Costs 3 Mana.</p>
-            </div>
-            <ul class="list-group text-center">
-              <li class="list-group-item align-items-center bg-orange" style="padding: .35rem 1.25rem;">
-                <span class="badge badge-light text-orange">2</span>
-                Weapon ATT
-              </li>
-            </ul>
-            <ul class="list-group text-center">
-              <li class="list-group-item align-items-center bg-orange" style="padding: .35rem 1.25rem;">
-                <span class="badge badge-light text-green">-4</span>
-                Enemy DEF
-              </li>
-            </ul>
-      </div>
-      </div>
-  </div>   
-  </div><!-- /.card -->    
-  </div><!-- /.accordion -->
-  </div><!-- /.row -->
+<?php include 'weapons/archer-longbow.php'; ?>
 
-</div>
+</div><!-- /.y-flex-container -->
 
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
 </body>
 
 </html>
