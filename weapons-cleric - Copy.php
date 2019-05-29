2566f32ad40070800cd6bffb48efa48ab17d064c
Rapid-SBS <info@rapid-sbs.com>
2019-05-29 15:38:09 -0700

    Cleric

--- weapons-cleric - Copy.php
+++ weapons-cleric - Copy.php
@@ -18,49 +18,49 @@
 <body>
 <div class="container pad-10">
 
-  <!-- Ice Stone-->
+  <!-- Mace & Shield -->
   <div class="row justify-content-center">
   <div class="col-4">
-  <div class="card text-white bg-blue mb-2" style="max-width: 18rem;">
-    <div class="accordion" id="accordion-ice">
+  <div class="card text-white bg-yellow mb-2" style="max-width: 18rem;">
+    <div class="accordion" id="accordion-maceshield">
       <div class="card-header text-center">
-        <h4>Ice Stone</h4>
+        <h4>Mace & Shield</h4>
       </div>
       <div class="card-header pad-5 text-center">
         <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
-          <h6>Ice Beam<h6>
+          <h6>Bludgeon<h6>
        </button>
     </div>
-      <div id="collapseThree" class="collapse hidden" aria-labelledby="headingThree" data-parent="#accordion-ice">
+      <div id="collapseThree" class="collapse hidden" aria-labelledby="headingThree" data-parent="#accordion-maceshield">
        <div class="card-body text-center">
-              <p class="card-text text-center">Fires a beam of subzero energy. <br> Critical Hits apply Black Ice debuff. <br>DMG = Weapon ATT + Player INT</p>
+              <p class="card-text text-center">Grants bonus DEF during combat. <br>DMG = Weapon ATT + Player ATT. </p>
             </div>
             <ul class="list-group text-center">
-              <li class="list-group-item align-items-center bg-blue" style="padding: .35rem 1.25rem;">
-                <span class="badge badge-light text-orange">3</span>
+              <li class="list-group-item align-items-center bg-yellow" style="padding: .35rem 1.25rem;">
+                <span class="badge badge-light text-orange">2</span>
                 Weapon ATT
+              </li>
+            </ul>
+            <ul class="list-group text-center">
+              <li class="list-group-item align-items-center bg-yellow" style="padding: .35rem 1.25rem;">
+                <span class="badge badge-light text-green">+2</span>
+                Bonus DEF
               </li>
             </ul>
       </div>
       <div class="card-header pad-5 text-center">
         <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
-            <h6>Glacial Barrier <span class="badge badge-light text-teal">3</span></h6>
+            <h6>Shield Ally <span class="badge badge-light text-teal">1</span></h6>
         </button>
       </div>
-      <div id="collapseFour" class="collapse hidden" aria-labelledby="headingFour" data-parent="#accordion-ice">
+      <div id="collapseFour" class="collapse hidden" aria-labelledby="headingFour" data-parent="#accordion-maceshield">
         <div class="card-body text-center">
-          <p class="card-text text-center">Summon a protective wall of ice. <br> Grants the user or a single ally bonus DEF and RES for one turn. <br>Costs 3 Mana.<br>
+          <p class="card-text text-center">Use your shield to protect <br>yourself or one ally. <br>Grants +4 DEF for one turn. <br>Costs 1 Mana.</p>
           </div>
           <ul class="list-group text-center">
-            <li class="list-group-item align-items-center bg-blue" style="padding: .35rem 1.25rem;">
+            <li class="list-group-item align-items-center bg-yellow" style="padding: .35rem 1.25rem;">
               <span class="badge badge-light text-green">+4</span>
               Bonus DEF
-            </li>
-          </ul>
-          <ul class="list-group text-center">
-            <li class="list-group-item align-items-center bg-blue" style="padding: .35rem 1.25rem;">
-              <span class="badge badge-light text-blue">+4</span>
-              Bonus RES
             </li>
           </ul>
       </div>
@@ -69,25 +69,25 @@
   </div><!-- /.accordion -->
   </div><!-- /.row -->
 
-  <!-- Fire-->
+  <!-- Quarterstaff -->
   <div class="row justify-content-center">
   <div class="col-4">
-  <div class="card text-white bg-blue mb-2" style="max-width: 18rem;">
+  <div class="card text-white bg-yellow mb-2" style="max-width: 18rem;">
     <div class="accordion" id="accordion-fire">
       <div class="card-header text-center">
-        <h4>Fire Stone</h4>
+        <h4>Quarterstaff</h4>
       </div>
       <div class="card-header pad-5 text-center">
         <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
-          <h6>Fireball<h6>
+          <h6>Downward Strike<h6>
        </button>
       </div>
       <div id="collapseFive" class="collapse hidden" aria-labelledby="headingFive" data-parent="#accordion-fire">
       <div class="card-body text-center">
-              <p class="card-text text-center">Hurl an orb of fire. <br>Critical Hits apply Burned debuff. <br>DMG = Weapon ATT + Player INT.</p>
+              <p class="card-text text-center">Strike your opponent. <br>DMG = Weapon ATT + Player ATT.</p>
       </div>
             <ul class="list-group text-center">
-              <li class="list-group-item align-items-center bg-blue" style="padding: .35rem 1.25rem;">
+              <li class="list-group-item align-items-center bg-yellow" style="padding: .35rem 1.25rem;">
                 <span class="badge badge-light text-orange">3</span>
                 Weapon ATT
               </li>
@@ -95,15 +95,15 @@
       </div>
       <div class="card-header pad-5 text-center">
         <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="true" aria-controls="collapseSix">
-            <h6>Inferno <span class="badge badge-light text-teal">4</span></h6>
+            <h6>Mend <span class="badge badge-light text-teal">2</span></h6>
         </button>
     </div>
     <div id="collapseSix" class="collapse hidden" aria-labelledby="headingSix" data-parent="#accordion-fire">
       <div class="card-body text-center">
-        <p class="card-text text-center">Applies Burn debuff to all enemies. <br>DMG = Weapon ATT. <br>Costs 4 Mana.</p>
+        <p class="card-text text-center">Heals a single ally. <br>Critical Hits heal twice the amount. <br>HEAL = Weapon ATT + Player INT.</p>
       </div>
         <ul class="list-group text-center">
-          <li class="list-group-item align-items-center bg-blue" style="padding: .35rem 1.25rem;">
+          <li class="list-group-item align-items-center bg-yellow" style="padding: .35rem 1.25rem;">
             <span class="badge badge-light text-orange">2</span>
             Weapon ATT
           </li>
@@ -115,42 +115,48 @@
   </div><!-- /.accordion -->
   </div><!-- /.row -->
 
-  <!-- Lightning Stone -->
+  <!-- Holy Tome -->
   <div class="row justify-content-center">
   <div class="col-4">
-  <div class="card text-white bg-blue mb-2" style="max-width: 18rem;">
-    <div class="accordion" id="accordion-lightning">
+  <div class="card text-white bg-yellow mb-2" style="max-width: 18rem;">
+    <div class="accordion" id="accordion-holytome">
       <div class="card-header text-center">
-        <h4>Lightning Stone</h4>
+        <h4>Holy Tome</h4>
       </div>
       <div class="card-header pad-5 text-center">
         <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          <h6>Lighting Bolt<h6>
+          <h6>Blessing<h6>
        </button>
     </div>
-      <div id="collapseOne" class="collapse hidden" aria-labelledby="headingOne" data-parent="#accordion-lightning">
+      <div id="collapseOne" class="collapse hidden" aria-labelledby="headingOne" data-parent="#accordion-holytome">
        <div class="card-body text-center">
-          <p class="card-text text-center">Hurl a bolt of electricity. <br>Critical Hits apply Shocked debuff. <br>DMG = Weapon ATT + Player INT. </p>
+          <p class="card-text text-center">Call for a blessing on your ally. <br>Boosts an ally's DEF and RES.</p>
         </div>
         <ul class="list-group text-center">
-          <li class="list-group-item align-items-center bg-blue" style="padding: .35rem 1.25rem;">
-            <span class="badge badge-light text-orange">3</span>
-            Weapon ATT
+          <li class="list-group-item align-items-center bg-yellow" style="padding: .35rem 1.25rem;">
+            <span class="badge badge-light text-green">+2</span>
+            Bonus DEF
+          </li>
+        </ul>
+        <ul class="list-group text-center">
+          <li class="list-group-item align-items-center bg-yellow" style="padding: .35rem 1.25rem;">
+            <span class="badge badge-light text-blue">+2</span>
+            Bonus RES
           </li>
         </ul>
       </div>
       <div class="card-header pad-5 text-center">
         <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-            <h6>Thunder Storm <span class="badge badge-light text-teal">4</span></h6>
+            <h6>Mass Healing <span class="badge badge-light text-teal">4</span></h6>
         </button>
     </div>
-      <div id="collapseTwo" class="collapse hidden" aria-labelledby="headingTwo" data-parent="#accordion-lightning">
+      <div id="collapseTwo" class="collapse hidden" aria-labelledby="headingTwo" data-parent="#accordion-holytome">
        <div class="card-body text-center">
-          <p class="card-text text-center">Rain lightning from the heavens. <br> Damages all enemies. <br>DMG = Weapon ATT + Player INT.<br>Costs 4 Mana.</p>
+          <p class="card-text text-center">Heals your entire party. <br>Critical Hits heal twice the amount. <br>HEAL = Weapon ATT. <br>Costs 5 Mana.</p>
         </div>
         <ul class="list-group text-center">
-          <li class="list-group-item align-items-center bg-blue" style="padding: .35rem 1.25rem;">
-            <span class="badge badge-light text-orange">2</span>
+          <li class="list-group-item align-items-center bg-yellow" style="padding: .35rem 1.25rem;">
+            <span class="badge badge-light text-orange">4</span>
             Weapon ATT
           </li>
         </ul>
