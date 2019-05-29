89a57ddd73afd5c976181f5366a5cacbd4aed6f5
Rapid-SBS <info@rapid-sbs.com>
2019-05-29 16:23:38 -0700

    Update monk.php

--- weapons/monk.php
+++ weapons/monk.php
@@ -1,5 +1,5 @@
   <!-- Bo Staff -->
- <div class="card wide-275 mar-20 text-white bg-orange">
+ <div class="card wide-275 mar-20 text-white bg-red">
   <div class="accordion" id="accordion-bostaff">
       <div class="card-header text-center">
         <h4>Bo Staff</h4>
@@ -14,8 +14,8 @@
           <p class="card-text text-center">Strike your opponent. <br>Critical Hits apply Stun debuff. <br>DMG = Weapon ATT + Player ATT.</p>
         </div>
         <ul class="list-group text-center">
-          <li class="list-group-item align-items-center light-transparency bg-orange" style="padding: .35rem 1.25rem;">
-            <span class="badge badge-light text-orange">3</span>
+          <li class="list-group-item align-items-center light-transparency bg-red" style="padding: .35rem 1.25rem;">
+            <span class="badge badge-light text-red">3</span>
               Weapon ATT
           </li>
         </ul>
@@ -28,9 +28,10 @@
       <div id="collapseTwo" class="collapse hidden" aria-labelledby="headingTwo" data-parent="#accordion-bostaff">
         <div class="card-body light-transparency pad-10 text-center">
           <p class="card-text text-center">Stun an enemy for one turn. <br>DMG = Weapon ATT + Player ATT. <br>Costs 2 Mana.</p>
+        </div>
         <ul class="list-group text-center">
-          <li class="list-group-item align-items-center light-transparency bg-orange" style="padding: .35rem 1.25rem;">
-            <span class="badge badge-light text-orange">1</span>
+          <li class="list-group-item align-items-center light-transparency bg-red" style="padding: .35rem 1.25rem;">
+            <span class="badge badge-light text-red">1</span>
             Weapon ATT
           </li>
         </ul>
@@ -39,7 +40,7 @@
   </div><!-- /.accordion -->
 
   <!-- Brawler's Fist-->
-  <div class="card wide-275 mar-20 text-white bg-orange">
+  <div class="card wide-275 mar-20 text-white bg-red">
   <div class="accordion" id="accordion-fist">
       <div class="card-header text-center">
         <h4>Brawler's Fist</h4>
@@ -54,8 +55,8 @@
           <p class="card-text text-center">Slam your fist into your opponent. <br>DMG = Weapon ATT + Player ATT</p>
         </div>
         <ul class="list-group text-center">
-          <li class="list-group-item align-items-center light-transparency bg-orange" style="padding: .35rem 1.25rem;">
-            <span class="badge badge-light text-orange">3</span>
+          <li class="list-group-item align-items-center light-transparency bg-red" style="padding: .35rem 1.25rem;">
+            <span class="badge badge-light text-red">3</span>
             Weapon ATT
           </li>
         </ul>
@@ -70,7 +71,7 @@
           <p class="card-text text-center">Launch a metaphysical attack. <br> Ignores all enemy RES. <br>DMG = Weapon ATT + Player INT. <br>Costs 3 Mana.</p>
         </div>
           <ul class="list-group text-center">
-            <li class="list-group-item align-items-center light-transparency bg-orange" style="padding: .35rem 1.25rem;">
+            <li class="list-group-item align-items-center light-transparency bg-red" style="padding: .35rem 1.25rem;">
               Enemy RES = <span class="badge badge-light text-blue">0</span>
             </li>
           </ul>
@@ -79,7 +80,7 @@
   </div><!-- /.accordion -->
 
   <!-- Wind Fu-->
-  <div class="card wide-275 mar-20 text-white bg-orange">
+  <div class="card wide-275 mar-20 text-white bg-red">
   <div class="accordion" id="accordion-windfu">
       <div class="card-header text-center">
         <h4>Wind Fu</h4>
@@ -94,8 +95,8 @@
           <p class="card-text text-center">Kick with tornado force. <br>Critical Hits apply Stun debuff. <br>DMG = Weapon ATT + Player ATT. </p>
         </div>
         <ul class="list-group text-center">
-          <li class="list-group-item align-items-center light-transparency bg-orange" style="padding: .35rem 1.25rem;">
-            <span class="badge badge-light text-orange">3</span>
+          <li class="list-group-item align-items-center light-transparency bg-red" style="padding: .35rem 1.25rem;">
+            <span class="badge badge-light text-red">3</span>
             Weapon ATT
           </li>
         </ul>
@@ -110,14 +111,14 @@
           <p class="card-text text-center">Fire hurricane-level winds from the palm of your hand. <br> Triple Critical Hit Chance. <br>DMG = Weapon ATT + Player INT.<br>Costs 3 Mana.</p>
         </div>
           <ul class="list-group text-center">
-          <li class="list-group-item align-items-center light-transparency bg-orange" style="padding: .35rem 1.25rem;">
-            <span class="badge badge-light text-orange">2</span>
+          <li class="list-group-item align-items-center light-transparency bg-red" style="padding: .35rem 1.25rem;">
+            <span class="badge badge-light text-red">2</span>
              Weapon ATT
           </li>
         </ul>
         <ul class="list-group text-center">
-          <li class="list-group-item align-items-center light-transparency bg-orange" style="padding: .35rem 1.25rem;">
-            <span class="badge badge-light text-orange">2</span>
+          <li class="list-group-item align-items-center light-transparency bg-red" style="padding: .35rem 1.25rem;">
+            <span class="badge badge-light text-red">2</span>
             Weapon ATT
           </li>
         </ul>
