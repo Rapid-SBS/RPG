d98e10899f60765d153e70b22a3c1ea258a0d800
Rapid-SBS <info@rapid-sbs.com>
2019-05-29 13:51:07 -0700

    weapons

--- weapons-archer.php
+++ weapons-archer.php
@@ -18,150 +18,15 @@
 
 <body>
 
-<div class="x-scroll-wrapper justify-content-center pad-10">
+<div class="y-flex-container pad-10">
 
-  <!-- Bow -->
-  <div class="card x-scroll-item mar-20 text-white bg-green">
-    <div class="accordion" id="accordion-bow">
-      <div class="card-header text-center">
-        <h4>Bow</h4>
-      </div>
-      <div class="card-header pad-5 text-center">
-        <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          <h6>Arrow</h6>
-        </button>
-      </div>
-      <div id="collapseOne" class="collapse hidden" aria-labelledby="headingOne" data-parent="#accordion-bow">
-        <div class="card-body text-center">
-          <p class="card-text text-center">Fires an arrow. <br>DMG = Weapon ATT + Player ATT. </p>
-        </div>
-        <ul class="list-group text-center">
-          <li class="list-group-item align-items-center bg-green" style="padding: .35rem 1.25rem;">
-            <span class="badge badge-light text-orange">2</span>
-            Weapon ATT
-          </li>
-        </ul>
-      </div>
-      <div class="card-header pad-5 text-center">
-        <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-            <h6>Doubleshot <span class="badge badge-light text-teal">2</span></h6>
-        </button>
-    </div>
-      <div id="collapseTwo" class="collapse hidden" aria-labelledby="headingTwo" data-parent="#accordion-bow">
-       <div class="card-body text-center">
-          <p class="card-text text-center">Fires two arrows. <br>DMG = 2 x (Weapon ATT + Player ATT).<br>Costs 2 Mana.</p>
-        </div>
-        <ul class="list-group text-center">
-          <li class="list-group-item align-items-center bg-green" style="padding: .35rem 1.25rem;">
-            <span class="badge badge-light text-orange">2</span>
-            Weapon ATT
-          </li>
-        </ul>
-      </div>
-  </div><!-- /.card -->    
-  </div><!-- /.accordion -->
+<?php include 'weapons/archer-bow.php'; ?>
 
-  <!-- Crossbow-->
-  <div class="card x-scroll-item mar-20 text-white bg-green">
-    <div class="accordion" id="accordion-crossbow">
-      <div class="card-header text-center">
-        <h4>Crossbow</h4>
-      </div>
-      <div class="card-header pad-5 text-center">
-        <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
-          <h6> Arrow</h6>
-       </button>
-    </div>
-      <div id="collapseThree" class="collapse hidden" aria-labelledby="headingThree" data-parent="#accordion-crossbow">
-       <div class="card-body text-center">
-              <p class="card-text text-center">Fires an arrow. <br>DMG = Weapon ATT + Player ATT. </p>
-            </div>
-            <ul class="list-group text-center">
-              <li class="list-group-item align-items-center bg-green" style="padding: .35rem 1.25rem;">
-                <span class="badge badge-light text-orange">3</span>
-                Weapon ATT
-              </li>
-            </ul>
-      </div>
-      <div class="card-header pad-5 text-center">
-        <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
-            <h6>Metal Arrow <span class="badge badge-light text-teal">3</span></h6>
-        </button>
-      </div>
-      <div id="collapseFour" class="collapse hidden" aria-labelledby="headingFour" data-parent="#accordion-crossbow">
-        <div class="card-body text-center">
-          <p class="card-text text-center">Pierces enemy defenses.<br>DMG = Weapon ATT + Player ATT. <br>Costs 3 Mana.</p>
-          </div>
-          <ul class="list-group text-center">
-            <li class="list-group-item align-items-center bg-green" style="padding: .35rem 1.25rem;">
-              <span class="badge badge-light text-orange">3</span>
-              Weapon ATT
-            </li>
-          </ul>
-          <ul class="list-group text-center">
-            <li class="list-group-item align-items-center bg-green" style="padding: .35rem 1.25rem;">
-              <span class="badge badge-light text-green">-4</span>
-              Enemy DEF
-            </li>
-          </ul>
-      </div>
-  </div><!-- /.card -->    
-  </div><!-- /.accordion -->
+<?php include 'weapons/archer-crossbow.php'; ?>
 
-  <!-- Longbow-->
-  <div class="card x-scroll-item mar-20 text-white bg-green">
-    <div class="accordion" id="accordion-longbow">
-      <div class="card-header text-center">
-        <h4>Longbow</h4>
-      </div>
-      <div class="card-header pad-5 text-center">
-        <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
-          <h6>Arrow</h6>
-       </button>
-      </div>
-      <div id="collapseFive" class="collapse hidden" aria-labelledby="headingFive" data-parent="#accordion-longbow">
-      <div class="card-body text-center">
-              <p class="card-text text-center">Fires an arrow. <br>DMG = Weapon ATT + Player ATT.</p>
-      </div>
-            <ul class="list-group text-center">
-              <li class="list-group-item align-items-center bg-green" style="padding: .35rem 1.25rem;">
-                <span class="badge badge-light text-orange">3</span>
-                Weapon ATT
-              </li>
-            </ul>
-      </div>
-      <div class="card-header pad-5 text-center">
-        <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="true" aria-controls="collapseSix">
-            <h6>Snipe <span class="badge badge-light text-teal">2</span></h6>
-        </button>
-    </div>
-    <div id="collapseSix" class="collapse hidden" aria-labelledby="headingSix" data-parent="#accordion-longbow">
-      <div class="card-body text-center">
-        <p class="card-text text-center">Double Critical Hit/Miss Chance. <br>DMG = Weapon ATT + Player ATT.<br>Costs 2 Mana.</p>
-      </div>
-        <ul class="list-group text-center">
-          <li class="list-group-item align-items-center bg-green" style="padding: .35rem 1.25rem;">
-            <span class="badge badge-light text-orange">3</span>
-            Weapon ATT
-          </li>
-        </ul>
-        <ul class="list-group text-center">
-          <li class="list-group-item align-items-center bg-green" style="padding: .35rem 1.25rem;">
-            <span class="badge badge-light text-teal">2x</span>
-            Crit Chance
-          </li>
-        </ul>
-        <ul class="list-group text-center">
-          <li class="list-group-item align-items-center bg-green" style="padding: .35rem 1.25rem;">
-            <span class="badge badge-light text-teal">2x</span>
-            Miss Chance
-          </li>
-        </ul>
-      </div>
-  </div><!-- /.card -->    
-  </div><!-- /.accordion -->
+<?php include 'weapons/archer-longbow.php'; ?>
 
-</div>
+</div><!-- /.y-flex-container -->
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
