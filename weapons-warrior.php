214337479b6098d5918bce994bb8dea07ce24c41
Rapid-SBS <info@rapid-sbs.com>
2019-05-29 14:57:44 -0700

    Wariror complete

--- weapons-warrior.php
+++ weapons-warrior.php
@@ -22,6 +22,10 @@
 
 <?php include 'weapons/warrior-sword.php'; ?>
 
+<?php include 'weapons/warrior-halberd.php'; ?>
+
+<?php include 'weapons/warrior-battleaxe.php'; ?>
+
 </div><!-- /.y-flex-container -->
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
---
+++ weapons/warrior-halberd.php
@@ -0,0 +1,40 @@
+  <!-- Halberd-->
+  <div class="card wide-275 mar-20 text-white bg-orange">
+  <div class="accordion" id="accordion-halberd">
+      <div class="card-header text-center">
+        <h4>Halberd</h4>
+      </div>
+      <div class="card-header pad-5 text-center">
+        <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+          <h6>Jab</h6>
+       </button>
+      </div>
+      <div id="collapseThree" class="collapse hidden" aria-labelledby="headingThree" data-parent="#accordion-halberd">
+        <div class="card-body light-transparency pad-10 text-center">
+          <p class="card-text text-center">Thrust your Halberd at an enemy. <br>DMG = Weapon ATT + Player ATT. </p>
+        </div>
+        <ul class="list-group text-center">
+          <li class="list-group-item align-items-center light-transparency bg-orange" style="padding: .35rem 1.25rem;">
+            <span class="badge badge-light text-orange">3</span>
+            Weapon ATT
+          </li>
+        </ul>
+      </div>
+      <div class="card-header pad-5 text-center">
+        <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+          <h6>Sweeping Blow <span class="badge badge-light text-teal">3</span></h6>
+        </button>
+      </div>
+      <div id="collapseFour" class="collapse hidden" aria-labelledby="headingFour" data-parent="#accordion-halberd">
+        <div class="card-body light-transparency pad-10 text-center">
+          <p class="card-text text-center">Attack all enemies. <br>DMG = Weapon ATT + Player ATT. <br>Costs 3 Mana.</p>
+        </div>
+          <ul class="list-group text-center">
+            <li class="list-group-item align-items-center light-transparency bg-orange" style="padding: .35rem 1.25rem;">
+              <span class="badge badge-light text-orange">3</span>
+                Weapon ATT
+            </li>
+          </ul>
+      </div>
+  </div><!-- /.card -->    
+  </div><!-- /.accordion -->
