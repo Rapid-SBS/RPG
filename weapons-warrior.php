74c9370b2c508de9cf3edc819b77bcf66848c454
Rapid-SBS <info@rapid-sbs.com>
2019-05-29 14:33:38 -0700

    Update weapons-warrior.php

--- weapons-warrior.php
+++ weapons-warrior.php
@@ -22,10 +22,6 @@
 
 <?php include 'weapons/weapons-warrior.php'; ?>
 
-<?php include 'weapons/archer-crossbow.php'; ?>
-
-<?php include 'weapons/archer-longbow.php'; ?>
-
 </div><!-- /.y-flex-container -->
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
