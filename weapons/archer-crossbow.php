70b14937c6380e844d86936d38a9fc489eb0b28d
Rapid-SBS <info@rapid-sbs.com>
2019-05-29 14:11:55 -0700

    Bows

--- weapons/archer-crossbow.php
+++ weapons/archer-crossbow.php
@@ -3,18 +3,18 @@
   <div class="accordion" id="accordion-crossbow">
       <div class="card-header text-center">
         <h4>Crossbow</h4>
-      </div><!-- /.card-header --> 
+      </div>
       <div class="card-header pad-5 text-center">
         <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
           <h6> Arrow</h6>
        </button>
-      </div><!-- /.card-header --> 
+      </div>
       <div id="collapseThree" class="collapse hidden" aria-labelledby="headingThree" data-parent="#accordion-crossbow">
-        <div class="card-body pad-10 text-center">
+        <div class="card-body light-transparency pad-10 text-center">
           <p class="card-text text-center">Fires an arrow. <br>DMG = Weapon ATT + Player ATT. </p>
         </div>
         <ul class="list-group text-center">
-          <li class="list-group-item align-items-center bg-green" style="padding: .35rem 1.25rem;">
+          <li class="list-group-item align-items-center light-transparency bg-green" style="padding: .35rem 1.25rem;">
             <span class="badge badge-light text-orange">3</span>
             Weapon ATT
           </li>
@@ -24,13 +24,13 @@
         <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
           <h6>Metal Arrow <span class="badge badge-light text-teal">3</span></h6>
         </button>
-      </div><!-- /.card-header --> 
+      </div>
       <div id="collapseFour" class="collapse hidden" aria-labelledby="headingFour" data-parent="#accordion-crossbow">
-        <div class="card-body pad-10 text-center">
+        <div class="card-body light-transparency pad-10 text-center">
           <p class="card-text text-center">Pierces enemy defenses.<br>DMG = Weapon ATT + Player ATT. <br>Costs 3 Mana.</p>
         </div>
           <ul class="list-group text-center">
-            <li class="list-group-item align-items-center bg-green" style="padding: .35rem 1.25rem;">
+            <li class="list-group-item align-items-center light-transparency bg-green" style="padding: .35rem 1.25rem;">
               <span class="badge badge-light text-orange">3</span>
               Weapon ATT
             </li>
