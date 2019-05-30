1ac70f3886b32f3cb3396517bdf449871346cc35
Rapid-SBS <info@rapid-sbs.com>
2019-05-30 08:37:28 -0700

    Update weapons-warrior.php

--- weapons-warrior.php
+++ weapons-warrior.php
@@ -6,9 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  
+  <script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
 
   <title>RPGenerator</title>
 
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="css/spacing.css">
   <link rel="stylesheet" type="text/css" href="css/colors.css">
   <link rel="stylesheet" type="text/css" href="css/style.css">
@@ -17,12 +23,18 @@
 
 <body>
 
+<div class="y-flex-container pad-10">
 
+<?php include 'weapons/warrior-sword.php'; ?>
 
 <button id="hide">Hide</button>
 <button id="show">Show</button>
 
-<p>If you click on the "Hide" button, I will disappear.</p>
+<p style="display: none;">If you click on the "Hide" button, I will disappear.</p>
+
+<?php include 'weapons/warrior-halberd.php'; ?>
+
+<?php include 'weapons/warrior-battleaxe.php'; ?>
 
 <script>
 $(document).ready(function(){
@@ -35,7 +47,7 @@ $(document).ready(function(){
 });
 </script>
 
-
+</div><!-- /.y-flex-container -->
 
 <footer class="footer mt-auto text-center bg-light">
   <div class="container pad-20">
@@ -43,7 +55,6 @@ $(document).ready(function(){
   </div>
 </footer>
 
-  <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
 </body>
