f72168d4a1dfff767e2d13e9ef9e6de4637782f8
Rapid-SBS <info@rapid-sbs.com>
2019-05-31 15:12:24 -0700

    Update weapons-archer.php

--- weapons/weapons-archer.php
+++ weapons/weapons-archer.php
@@ -1,14 +1,14 @@
 <?php include 'archer-bow.php'; ?>
   
-  <button type="button" onclick="setWeaponBow()" class="btn btn-primary rpg-wpn-select-btn" id="rpg-wpn-btn-bow" style="display: none;"><span class="pulsing-text-white">Select</span></button>
+  <button type="button" onclick="setWeaponBow()" class="btn btn-primary rpg-wpn-select-btn" id="rpg-wpn-btn-bow" href="skill-select.php" style="display: none;"><span class="pulsing-text-white">Select</span></button>
 
 <?php include 'archer-crossbow.php'; ?>
 
-  <button type="button" onclick="setWeaponCrossbow()" class="btn btn-primary rpg-wpn-select-btn" id="rpg-wpn-btn-crossbow" style="display: none;"><span class="pulsing-text-white">Select</span></button>
+  <button type="button" onclick="setWeaponCrossbow()" class="btn btn-primary rpg-wpn-select-btn" id="rpg-wpn-btn-crossbow" href="skill-select.php" style="display: none;"><span class="pulsing-text-white">Select</span></button>
 
 <?php include 'archer-longbow.php'; ?>
 
-  <button type="button" onclick="setWeaponLongbow()" class="btn btn-primary rpg-wpn-select-btn" id="rpg-wpn-btn-longbow" style="display: none;"><span class="pulsing-text-white">Select</span></button>
+  <button type="button" onclick="setWeaponLongbow()" class="btn btn-primary rpg-wpn-select-btn" id="rpg-wpn-btn-longbow" href="skill-select.php" style="display: none;"><span class="pulsing-text-white">Select</span></button>
 
 <script> 
 var rpgWeapon = localStorage.getItem('rpgWeapon');
