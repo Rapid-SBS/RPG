1563d43ce88782922b93f9799e2f4978868b7e0e
Rapid-SBS <info@rapid-sbs.com>
2019-05-31 12:49:31 -0700

    Updates

--- skills/skill-template.php
+++ skills/skill-template.php
@@ -1,42 +1,25 @@
 <div class="row">
   <!-- Mark Enemy -->
-  <a class="btn text-white pad-10 mar-5 bg-green" onclick="collapseMark()" href="#collapseMark">
+  <a class="btn text-white pad-10 mar-5 bg-green" href="#collapseMark" role="button" aria-expanded="false" aria-controls="collapseMark">
     <h6>Mark Enemy <span class="badge badge-light text-teal">2</span></h6>
   </a>
   <!-- Salve -->
-  <a class="btn text-white pad-10 mar-5 bg-green" onclick="collapseSalve()" href="#collapseSalve">
+  <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseSalve" role="button" aria-expanded="false" aria-controls="collapseSalve">
     <h6>Salve <span class="badge badge-light text-teal">2</span></h6>
   </a>
 </div><!-- /.row -->
 
 <div id="support-archer" class="row">
   <!-- Mark Enemy -->
-  <div class="wide-275" id="collapseMark">
+  <div class="collapse mx-auto wide-275" id="collapseMark" data-parent="#support-archer">
     <div class="card card-body light-transparency">
       Lowers enemy DEF for one turn. <br> Costs 2 Mana.
     </div>
   </div>
   <!-- Salve -->
-  <div class="wide-275" id="collapseSalve">
+  <div class="collapse mx-auto wide-275" id="collapseSalve" data-parent="#support-archer">
     <div class="card card-body light-transparency">
       Heal yourself or one ally. <br> Costs 2 Mana.
     </div>
   </div>
 </div><!-- /.row -->
-
-<script>
-function collapseMark() {
-  $("#collapseMark").addClass("js-collapse-show");
-  $("#collapseMark").removeClass("js-collapse-hide");
-  $("#collapseSalve").addClass("js-collapse-hide");
-  $("#collapseSalve").removeClass("js-collapse-show");
-}
-
-function collapseSalve() {
-  $("#collapseSalve").addClass("js-collapse-show");
-  $("#collapseSalve").removeClass("js-collapse-hide");
-  $("#collapseMark").addClass("js-collapse-hide");
-  $("#collapseMark").removeClass("js-collapse-show");
-}
-
-</script> 
