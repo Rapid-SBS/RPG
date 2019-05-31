e876fb46e20f13b46644c1ce07c17904272ac91d
Rapid-SBS <info@rapid-sbs.com>
2019-05-30 23:19:26 -0700

    Rulebook

--- index.php
+++ index.php
@@ -277,9 +277,13 @@ $(document).ready(function(){
 
 <footer class="footer mt-auto text-center bg-light">
   <div class="container pad-20">
+    <div class="pad-b-20">
+      <a class="btn btn-dark text-white mar-r-10 mar-l-10" href="http://rapid-phyre.net" role="button">Rulebook</a>
+    </div>
     <span class="text-muted">&copy;RPGenius 2019. All Rights Reserved.</span>
   </div>
 </footer>
+
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
