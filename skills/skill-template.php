d9f1a57f3638cae68a187d374d8364984cb61a3e
Rapid-SBS <info@rapid-sbs.com>
2019-05-31 13:27:38 -0700

    Update skill-template.php

--- skills/skill-template.php
+++ skills/skill-template.php
@@ -1,48 +1,25 @@
 <div class="row">
   <!-- Mark Enemy -->
-  <a class="btn text-white pad-10 mar-5 bg-green" onclick="showMark()" onblur="hideMark()" href="#collapseMark">
+  <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseMark" role="button" aria-expanded="false" aria-controls="collapseMark">
     <h6>Mark Enemy <span class="badge badge-light text-teal">2</span></h6>
   </a>
   <!-- Salve -->
-  <a class="btn text-white pad-10 mar-5 bg-green" onclick="showSalve()" onblur="hideSalve()" href="#collapseSalve">
+  <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseSalve" role="button" aria-expanded="false" aria-controls="collapseSalve">
     <h6>Salve <span class="badge badge-light text-teal">2</span></h6>
   </a>
 </div><!-- /.row -->
 
 <div id="support-archer" class="row">
   <!-- Mark Enemy -->
-  <div class="wide-275" id="collapseMark">
+  <div class="collapse mx-auto wide-275" id="collapseMark" data-parent="#support-archer">
     <div class="card card-body light-transparency">
       Lowers enemy DEF for one turn. <br> Costs 2 Mana.
     </div>
   </div>
   <!-- Salve -->
-  <div class="wide-275" id="collapseSalve">
+  <div class="collapse mx-auto wide-275" id="collapseSalve" data-parent="#support-archer">
     <div class="card card-body light-transparency">
       Heal yourself or one ally. <br> Costs 2 Mana.
     </div>
   </div>
 </div><!-- /.row -->
-
-<script>
-function showMark() {
-  $("#collapseMark").addClass("js-collapse-show");
-  $("#collapseMark").removeClass("js-collapse-hide");
-}
-
-function showSalve() {
-  $("#collapseSalve").addClass("js-collapse-show");
-  $("#collapseSalve").removeClass("js-collapse-hide");
-}
-
-function hideMark() {
-  $("#collapseMark").addClass("js-collapse-hide");
-  $("#collapseMark").removeClass("js-collapse-show");
-}
-
-function hideSalve() {
-  $("#collapseSalve").addClass("js-collapse-hide");
-  $("#collapseSalve").removeClass("js-collapse-show");
-}
-
-</script> 
