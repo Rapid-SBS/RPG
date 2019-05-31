ebd3d83421d7b0d85b66a5316e8640fa851107d8
Rapid-SBS <info@rapid-sbs.com>
2019-05-31 15:32:32 -0700

    Ultimates

--- skills/skill-tree-archer.php
+++ skills/skill-tree-archer.php
@@ -1,5 +1,7 @@
 <div id="skill-tree-archer" style="width: 100%;">
 
+<?php include 'skills-archer-lvl-9.php'; ?>
+
 <?php include 'skills-archer-lvl-6.php'; ?>
 
 <?php include 'skills-archer-lvl-3.php'; ?>
---
+++ skills/skills-archer-lvl-9.php
@@ -0,0 +1,53 @@
+<div id="rpg-skills-archer-lvl-9" class="border-top pad-20">
+  
+  <div class="row pad-10"><h4 class="mx-auto">LEVEL 9</h4></div>
+
+  <div class="row justify-content-center">
+
+    <!-- Arrow Storm -->
+    <a class="btn text-white pad-10 mar-5 bg-blue" data-toggle="collapse" href="#collapseArrowstorm" role="button" aria-expanded="false" aria-controls="collapseArrowstorm">
+      <h6>Arrow Storm <span class="badge badge-light text-teal">11</span></h6>
+    </a>
+
+    <!-- Missile Salvo -->  
+    <a class="btn text-white pad-10 mar-5 bg-blue" data-toggle="collapse" href="#collapseMissilesalvo" role="button" aria-expanded="false" aria-controls="collapseMissilesalvo">
+      <h6>Missile Salvo <span class="badge badge-light text-teal">9</span></h6>
+    </a>
+
+  </div><!-- /.row -->
+
+  <div id="ultimate-archer" class="row mx-auto pad-10">
+
+    <!-- Arrow Storm -->
+    <div class="collapse mx-auto" id="collapseArrowstorm" data-parent="#ultimate-archer">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Ultimate Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Hail arrows down upon all your enemies.<br> DMG = Weapon ATT + Player ATT.<br> Costs 11 Mana.</p>
+        </div>
+        <ul class="list-group list-group-flush text-center">
+          <li class="list-group-item bg-light align-items-center" style="padding: .35rem 1.25rem;">
+            <span class="badge bg-white text-orange">10</span>
+            Weapon ATT
+          </li>
+        </ul>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+    <!-- Missile Salvo -->
+    <div class="collapse mx-auto" id="collapseMissilesalvo" data-parent="#ultimate-archer">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Ultimate Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Fire a deadly stream of arrows at one enemy.<br>DMG = 4 x (Player ATT)<br> Costs 9 Mana.</p>
+        </div>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+  </div><!-- /.row -->
+
+</div><!-- /.container -->
