42fb308d5a44c5193fcb7fe106507a6527666aa0
Rapid-SBS <info@rapid-sbs.com>
2019-05-31 10:18:47 -0700

    Update skill-template.php

--- skills/skill-template.php
+++ skills/skill-template.php
@@ -1,16 +1,16 @@
 <div class="row">
   <!-- Mark Enemy -->
-  <div class="rpg-wpn-bow card wide-160 text-white bg-green">
-  <div class="accordion" id="accordion-bow">
+  <div class="rpg-wpn-bow card wide-160 mar-5 text-white bg-green">
+  <div class="accordion" id="accordion-Mark">
       <div class="card-header text-center">
         <h4>Support</h4>
       </div><!-- /.card-header --> 
       <div class="card-header pad-5 text-center">
-        <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+        <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
           <h6>Mark Enemy <span class="badge badge-light text-teal">2</span></h6>
         </button>
       </div><!-- /.card-header --> 
-      <div id="collapseTwo" class="collapse hidden" aria-labelledby="headingTwo" data-parent="#accordion-bow">
+      <div id="collapseTwo" class="collapse hidden" aria-labelledby="headingTwo" data-parent="#accordion-Mark">
         <div class="card-body light-transparency pad-10 text-center">
           <p class="card-text text-center">Lowers enemy DEF <br>for one turn. <br>Costs 2 Mana.</p>
         </div><!-- /.card-body pad-10 --> 
@@ -25,8 +25,8 @@
   </div><!-- /.accordion -->
 
   <!-- Salve -->
-  <div class="rpg-wpn-bow card wide-160 text-white bg-green">
-  <div class="accordion" id="accordion-bow">
+  <div class="rpg-wpn-bow card wide-160 mar-5 text-white bg-green">
+  <div class="accordion" id="accordion-Salve">
       <div class="card-header text-center">
         <h4>Support</h4>
       </div><!-- /.card-header --> 
@@ -35,9 +35,9 @@
           <h6>Salve <span class="badge badge-light text-teal">2</span></h6>
         </button>
       </div><!-- /.card-header --> 
-      <div id="collapseTwo" class="collapse hidden" aria-labelledby="headingTwo" data-parent="#accordion-bow">
+      <div id="collapseTwo" class="collapse hidden" aria-labelledby="headingTwo" data-parent="#accordion-Salve">
         <div class="card-body light-transparency pad-10 text-center">
-          <p class="card-text text-center">Heal yourself <br>or an ally. <br>HEAL = Weapon ATT. <br>Costs 2 Mana.</p>
+          <p class="card-text text-center">Heal any ally. <br>HEAL = Weapon ATT. <br>Costs 2 Mana.</p>
         </div><!-- /.card-body pad-10 --> 
         <ul class="list-group text-center">
           <li class="list-group-item align-items-center light-transparency bg-green" style="padding: .35rem 1.25rem;">
