d3495116f140d9f44a1751376a04899f68298824
Rapid-SBS <info@rapid-sbs.com>
2019-05-31 11:00:20 -0700

    Update skill-template.php

--- skills/skill-template.php
+++ skills/skill-template.php
@@ -1,37 +1,25 @@
 <div class="row">
   <!-- Mark Enemy -->
-  <a class="btn text-white wide-160 bg-green" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+  <a class="btn text-white wide-160 mar-5 bg-green" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
     <h6>Mark Enemy <span class="badge badge-light text-teal">2</span></h6>
   </a>
+  <!-- Salve -->
+  <a class="btn text-white wide-160 mar-5 bg-green" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+    <h6>Salve <span class="badge badge-light text-teal">2</span></h6>
+  </a>
+</div><!-- /.row -->
 
+<div class="row">
+  <!-- Mark Enemy -->
   <div class="collapse wide-275" id="collapseExample">
     <div class="card card-body light-transparency">
       Lowers enemy DEF for one turn. <br> Costs 2 Mana.
     </div>
   </div>
-  <!-- Salve
-  <div class="rpg-wpn-bow card wide-160 mar-5 text-white bg-green">
-  <div class="accordion" id="accordion-Salve">
-      <div class="card-header text-center">
-        <h4>Support</h4>
-      </div> 
-      <div class="card-header pad-5 text-center">
-        <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-          <h6>Salve <span class="badge badge-light text-teal">2</span></h6>
-        </button>
-      </div>
-      <div id="collapseTwo" class="collapse hidden" aria-labelledby="headingTwo" data-parent="#accordion-Salve">
-        <div class="card-body light-transparency pad-10 text-center">
-          <p class="card-text text-center">Heal any ally. <br>HEAL = Weapon ATT. <br>Costs 2 Mana.</p>
-        </div> 
-        <ul class="list-group text-center">
-          <li class="list-group-item align-items-center light-transparency bg-green" style="padding: .35rem 1.25rem;">
-            <span class="badge badge-light text-orange">2</span>
-            Weapon ATT
-          </li>
-        </ul>
-      </div>
-  </div> 
-  </div> -->
-
+  <!-- Salve -->
+  <div class="collapse wide-275" id="collapseExample">
+    <div class="card card-body light-transparency">
+      Heal yourself or one ally. <br> Costs 2 Mana.
+    </div>
+  </div>
 </div><!-- /.row -->
