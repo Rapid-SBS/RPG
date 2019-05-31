6a115ce4b2dd025a2558d1bd1b4aaf9c564d4193
Rapid-SBS <info@rapid-sbs.com>
2019-05-31 11:03:03 -0700

    Update skill-template.php

--- skills/skill-template.php
+++ skills/skill-template.php
@@ -1,23 +1,23 @@
 <div class="row">
   <!-- Mark Enemy -->
-  <a class="btn text-white wide-160 mar-5 bg-green" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+  <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseMark" role="button" aria-expanded="false" aria-controls="collapseMark">
     <h6>Mark Enemy <span class="badge badge-light text-teal">2</span></h6>
   </a>
   <!-- Salve -->
-  <a class="btn text-white wide-160 mar-5 bg-green" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+  <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseSalve" role="button" aria-expanded="false" aria-controls="collapseSalve">
     <h6>Salve <span class="badge badge-light text-teal">2</span></h6>
   </a>
 </div><!-- /.row -->
 
 <div class="row">
   <!-- Mark Enemy -->
-  <div class="collapse wide-275" id="collapseExample">
+  <div class="collapse wide-275" id="collapseMark">
     <div class="card card-body light-transparency">
       Lowers enemy DEF for one turn. <br> Costs 2 Mana.
     </div>
   </div>
   <!-- Salve -->
-  <div class="collapse wide-275" id="collapseExample">
+  <div class="collapse wide-275" id="collapseSalve">
     <div class="card card-body light-transparency">
       Heal yourself or one ally. <br> Costs 2 Mana.
     </div>
