7bbd1d3301006d04cc3af0393c4d3fb723c0a537
Rapid-SBS <info@rapid-sbs.com>
2019-05-31 12:37:50 -0700

    js-collapse

--- skills/skill-template.php
+++ skills/skill-template.php
@@ -1,25 +1,42 @@
 <div class="row">
   <!-- Mark Enemy -->
-  <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseMark" role="button" aria-expanded="false" aria-controls="collapseMark">
+  <a class="btn text-white pad-10 mar-5 bg-green" onclick="collapseMark()" href="#collapseMark">
     <h6>Mark Enemy <span class="badge badge-light text-teal">2</span></h6>
   </a>
   <!-- Salve -->
-  <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseSalve" role="button" aria-expanded="false" aria-controls="collapseSalve">
+  <a class="btn text-white pad-10 mar-5 bg-green" onclick="collapseSalve()" href="#collapseSalve">
     <h6>Salve <span class="badge badge-light text-teal">2</span></h6>
   </a>
 </div><!-- /.row -->
 
 <div id="support-archer" class="row">
   <!-- Mark Enemy -->
-  <div class="collapse wide-275" id="collapseMark" data-parent="#support-archer">
+  <div class="wide-275" style="display: none;" id="collapseMark">
     <div class="card card-body light-transparency">
       Lowers enemy DEF for one turn. <br> Costs 2 Mana.
     </div>
   </div>
   <!-- Salve -->
-  <div class="collapse wide-275" id="collapseSalve" data-parent="#support-archer">
+  <div class="wide-275" style="display: none;" id="collapseSalve">
     <div class="card card-body light-transparency">
       Heal yourself or one ally. <br> Costs 2 Mana.
     </div>
   </div>
 </div><!-- /.row -->
+
+<script>
+function collapseMark() {
+  $("#collapseMark").addClass("js-collapse-show");
+  $("#collapseMark").removeClass("js-collapse-hide");
+  $("#collapseSalve").addClass("js-collapse-hide");
+  $("#collapseSalve").removeClass("js-collapse-show");
+}
+
+function collapseSalve() {
+  $("#collapseSalve").addClass("js-collapse-show");
+  $("#collapseSalve").removeClass("js-collapse-hide");
+  $("#collapseMark").addClass("js-collapse-hide");
+  $("#collapseMark").removeClass("js-collapse-show");
+}
+
+</script> 
