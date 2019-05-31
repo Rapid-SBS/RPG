052f4b0488e2f4e8d84dfae6dc5ae71d877e8492
Rapid-SBS <info@rapid-sbs.com>
2019-05-31 10:15:54 -0700

    Update skill-template.php

--- skills/skill-template.php
+++ skills/skill-template.php
@@ -1,3 +1,4 @@
+<div class="row">
   <!-- Mark Enemy -->
   <div class="rpg-wpn-bow card wide-160 text-white bg-green">
   <div class="accordion" id="accordion-bow">
@@ -22,3 +23,30 @@
       </div><!-- /.collapseTwo --> 
   </div><!-- /.card -->    
   </div><!-- /.accordion -->
+
+  <!-- Salve -->
+  <div class="rpg-wpn-bow card wide-160 text-white bg-green">
+  <div class="accordion" id="accordion-bow">
+      <div class="card-header text-center">
+        <h4>Support</h4>
+      </div><!-- /.card-header --> 
+      <div class="card-header pad-5 text-center">
+        <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          <h6>Salve <span class="badge badge-light text-teal">2</span></h6>
+        </button>
+      </div><!-- /.card-header --> 
+      <div id="collapseTwo" class="collapse hidden" aria-labelledby="headingTwo" data-parent="#accordion-bow">
+        <div class="card-body light-transparency pad-10 text-center">
+          <p class="card-text text-center">Heal yourself <br>or an ally. <br>HEAL = Weapon ATT. <br>Costs 2 Mana.</p>
+        </div><!-- /.card-body pad-10 --> 
+        <ul class="list-group text-center">
+          <li class="list-group-item align-items-center light-transparency bg-green" style="padding: .35rem 1.25rem;">
+            <span class="badge badge-light text-orange">2</span>
+            Weapon ATT
+          </li>
+        </ul>
+      </div><!-- /.collapseTwo --> 
+  </div><!-- /.card -->    
+  </div><!-- /.accordion -->
+
+</div><!-- /.row -->
