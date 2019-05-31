2dd2cc46f7a14234dd57ed4885f6bca712f785d1
Rapid-SBS <info@rapid-sbs.com>
2019-05-31 10:42:07 -0700

    Update skill-template.php

--- skills/skill-template.php
+++ skills/skill-template.php
@@ -3,29 +3,29 @@
   <a tabindex="0" class="btn btn-lg wide-160 text-white bg-green" role="button" data-toggle="popover" data-trigger="focus" title="Support Ability" data-content="Lowers enemy DEF for one turn. Costs 2 Mana."><h6>Mark Enemy <span class="badge badge-light text-teal">2</span></h6></a>
 
 
-  <!-- Salve -->
+  <!-- Salve
   <div class="rpg-wpn-bow card wide-160 mar-5 text-white bg-green">
   <div class="accordion" id="accordion-Salve">
       <div class="card-header text-center">
         <h4>Support</h4>
-      </div><!-- /.card-header --> 
+      </div> 
       <div class="card-header pad-5 text-center">
         <button class="btn btn-link text-white text-decoration-none" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
           <h6>Salve <span class="badge badge-light text-teal">2</span></h6>
         </button>
-      </div><!-- /.card-header --> 
+      </div>
       <div id="collapseTwo" class="collapse hidden" aria-labelledby="headingTwo" data-parent="#accordion-Salve">
         <div class="card-body light-transparency pad-10 text-center">
           <p class="card-text text-center">Heal any ally. <br>HEAL = Weapon ATT. <br>Costs 2 Mana.</p>
-        </div><!-- /.card-body pad-10 --> 
+        </div> 
         <ul class="list-group text-center">
           <li class="list-group-item align-items-center light-transparency bg-green" style="padding: .35rem 1.25rem;">
             <span class="badge badge-light text-orange">2</span>
             Weapon ATT
           </li>
         </ul>
-      </div><!-- /.collapseTwo --> 
-  </div><!-- /.card -->    
-  </div><!-- /.accordion -->
+      </div>
+  </div> 
+  </div> -->
 
 </div><!-- /.row -->
