bd48582fd9b9f8093dd5e4e7e586ea55fc223ee1
Rapid-SBS <info@rapid-sbs.com>
2019-05-31 17:33:13 -0700

    Update skills-archer-lvl-1.php

--- skills/skills-archer-lvl-1.php
+++ skills/skills-archer-lvl-1.php
@@ -4,10 +4,25 @@
 
     <?php include 'skills-vitality-1.php'; ?>
 
+  </div><!-- /.row -->
+
+</div><!-- /.container -->
+
+<div id="rpg-skills-archer-lvl-1" class="border-top pad-20">
+  
+  <div class="row pad-10"><h4 class="mx-auto">LEVEL 1</h4></div>
+
     <?php include 'skills-vitality-2.php'; ?>
 
-    <?php include 'skills-vitality-3.php'; ?>
+  </div><!-- /.row -->
 
+</div><!-- /.container -->
+
+<div id="rpg-skills-archer-lvl-1" class="border-top pad-20">
+  
+  <div class="row pad-10"><h4 class="mx-auto">LEVEL 1</h4></div>
+
+    <?php include 'skills-vitality-3.php'; ?>
 
   </div><!-- /.row -->
 
