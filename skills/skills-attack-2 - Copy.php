46fc292c0aceee2f080b4983ce17f777a65947a9
Rapid-SBS <info@rapid-sbs.com>
2019-05-31 19:00:22 -0700

    Up Arrow

--- skills/skills-attack-2 - Copy.php
+++ skills/skills-attack-2 - Copy.php
@@ -1,16 +1,16 @@
   <div class="row justify-content-center">
 
-    <!-- Attack 1 -->
-    <a class="btn text-white pad-10 mar-5 bg-red" data-toggle="collapse" href="#collapseAttack1" role="button" aria-expanded="false" aria-controls="collapseAttack1">
-      <h6>ATT &#8679;</h6>
+    <!-- Attack 2 -->
+    <a class="btn text-white pad-10 mar-5 bg-red" data-toggle="collapse" href="#collapseAttack2" role="button" aria-expanded="false" aria-controls="collapseAttack2">
+      <h6>Attack 2</h6>
     </a>
 
   </div><!-- /.row -->
 
-  <div id="attack-1" class="row mx-auto pad-10">
+  <div id="attack-2" class="row mx-auto pad-10">
 
-    <!-- Attack 1 -->
-    <div class="collapse mx-auto" id="collapseAttack1" data-parent="#attack-1">
+    <!-- Attack 2 -->
+    <div class="collapse mx-auto" id="collapseAttack2" data-parent="#attack-2">
       <div class="card bg-light">
         <div class="card-header text-center">
           <h6>Stat Increase</h6>
--- skills/skills-attack-3 - Copy.php
+++ skills/skills-attack-3 - Copy.php
@@ -1,16 +1,16 @@
   <div class="row justify-content-center">
 
-    <!-- Attack 1 -->
-    <a class="btn text-white pad-10 mar-5 bg-red" data-toggle="collapse" href="#collapseAttack1" role="button" aria-expanded="false" aria-controls="collapseAttack1">
-      <h6>ATT &#8679;</h6>
+    <!-- Attack 3 -->
+    <a class="btn text-white pad-10 mar-5 bg-red" data-toggle="collapse" href="#collapseAttack3" role="button" aria-expanded="false" aria-controls="collapseAttack3">
+      <h6>Attack 3</h6>
     </a>
 
   </div><!-- /.row -->
 
-  <div id="attack-1" class="row mx-auto pad-10">
+  <div id="attack-3" class="row mx-auto pad-10">
 
-    <!-- Attack 1 -->
-    <div class="collapse mx-auto" id="collapseAttack1" data-parent="#attack-1">
+    <!-- Attack 3 -->
+    <div class="collapse mx-auto" id="collapseAttack3" data-parent="#attack-3">
       <div class="card bg-light">
         <div class="card-header text-center">
           <h6>Stat Increase</h6>
