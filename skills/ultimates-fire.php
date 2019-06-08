b00df4eb98da40f76c06fb2a1c02d5960d51364c
Rapid-SBS <info@rapid-sbs.com>
2019-06-08 16:05:17 -0700

    Updates

---
+++ skills/ultimates-fire.php
@@ -0,0 +1,42 @@
+  <div class="row justify-content-center">
+
+    <!-- Plasma Beam -->  
+    <a class="btn text-white pad-10 mar-5 bg-purple" data-toggle="collapse" href="#collapsePlasmabeam" role="button" aria-expanded="false" aria-controls="collapsePlasmabeam">
+      <h6>Plasma Beam <span class="badge badge-light text-teal">9</span></h6>
+    </a>
+
+    <!-- Meteor Strike -->
+    <a class="btn text-white pad-10 mar-5 bg-purple" data-toggle="collapse" href="#collapseMeteorstrike" role="button" aria-expanded="false" aria-controls="collapseMeteorstrike">
+      <h6>Meteor Strike <span class="badge badge-light text-teal">12</span></h6>
+    </a>
+
+
+  </div><!-- /.row -->
+
+  <div id="ultimate-fire" class="row mx-auto pad-10">
+
+    <!-- Plasma Beam -->
+    <div class="collapse mx-auto" id="collapsePlasmabeam" data-parent="#ultimate-fire">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Ultimate Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Fire a beam so hot that it<br>pierces all enemy defenses.<br> Ignores all enemy RES. <br> DMG = 3 x (Player INT).<br> Costs 9 Mana.</p> 
+        </div>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+    <!-- Meteor Strike -->
+    <div class="collapse mx-auto" id="collapseMeteor Strike" data-parent="#ultimate-fire">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Ultimate Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Summon a meteor from the heavens<br>to crash into your enemies. <br> DMG = 4 x (Player INT).<br> Costs 14 Mana.</p>
+        </div>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+  </div><!-- /.row -->
