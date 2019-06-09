a0bb1c3489fc7fa1bc7507a9a45a91c858b1773c
Rapid-SBS <info@rapid-sbs.com>
2019-06-08 21:46:06 -0700

    Create ultimates-quarterstaff.php

---
+++ skills/ultimates-quarterstaff.php
@@ -0,0 +1,52 @@
+  <div class="row justify-content-center">
+
+    <!-- Skull Bash -->
+    <a class="btn text-white pad-10 mar-5 bg-purple" data-toggle="collapse" href="#collapseSkullbash" role="button" aria-expanded="false" aria-controls="collapseSkullbash">
+      <h6>Skull Bash <span class="badge badge-light text-teal">8</span></h6>
+    </a>
+
+    <!-- Revive -->  
+    <a class="btn text-white pad-10 mar-5 bg-purple" data-toggle="collapse" href="#collapseRevive" role="button" aria-expanded="false" aria-controls="collapseRevive">
+      <h6>Revive <span class="badge badge-light text-teal">12</span></h6>
+    </a>
+
+  </div><!-- /.row -->
+
+  <div id="ultimate-quarterstaff" class="row mx-auto pad-10">
+
+    <!-- Skull Bash -->
+    <div class="collapse mx-auto" id="collapseSkullbash" data-parent="#ultimate-quarterstaff">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Ultimate Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Slam your quarterstaff into<br>the skull of an enemy. <br>Double Critical Hit Chance.<br> DMG = Weapon ATT + Player ATT.<br> Costs 8 Mana.</p>
+        </div>
+        <ul class="list-group list-group-flush text-center">
+          <li class="list-group-item bg-light align-items-center" style="padding: .35rem 1.25rem;">
+            <span class="badge bg-white text-orange">13</span>
+            Weapon ATT
+          </li>
+        </ul>
+        <ul class="list-group list-group-flush text-center">
+          <li class="list-group-item bg-light align-items-center" style="padding: .35rem 1.25rem;">
+            <span class="badge bg-white text-teal">2x</span>
+            Crit Chance
+          </li>
+        </ul>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+    <!-- Revive -->
+    <div class="collapse mx-auto" id="collapseRevive" data-parent="#ultimate-quarterstaff">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Ultimate Ability</h6>
+        </div>
+        <div class="card-body pad-5">Revive a fallen ally with full HP.<br> Costs 12 Mana.</p>
+        </div>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+  </div><!-- /.row -->
