1f648d2a2589b5befe861f25c837e3fdef68b8f5
Rapid-SBS <info@rapid-sbs.com>
2019-06-11 09:41:57 -0700

    UPdates

---
+++ skills/supports-longbow.php
@@ -0,0 +1,41 @@
+  <div class="row justify-content-center">
+
+    <!-- Targeted Aim -->
+    <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseTargetedaim" role="button" aria-expanded="false" aria-controls="collapseTargetedaim">
+      <h6>Targeted Aim <span class="badge badge-light text-teal">2</span></h6>
+    </a>
+
+    <!-- Nature's Blessing -->  
+    <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseNaturesblessing" role="button" aria-expanded="false" aria-controls="collapseNaturesblessing">
+      <h6>Nature's Blessing <span class="badge badge-light text-teal">6</span></h6>
+    </a>
+
+  </div><!-- /.row -->
+
+  <div id="support-longbow" class="row mx-auto pad-10">
+
+    <!-- Targeted Aim -->
+    <div class="collapse mx-auto" id="collapseTargetedaim" data-parent="#support-longbow">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Support Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Target an enemy's weakness. Increases Critical<br>Hit Rate of yourself or an ally by 1x<br> (Up to a maximum of 3x) for 1 turn.<br> Costs 2 Mana.</p>
+        </div>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+    <!-- Nature's Blessing -->
+    <div class="collapse mx-auto" id="collapseNaturesblessing" data-parent="#support-longbow">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Support Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Draw upon your connection with<br>nature to heal your allies.<br>HEAL = Player INT.<br> Costs 3 Mana.</p>
+        </div>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+  </div><!-- /.row -->
