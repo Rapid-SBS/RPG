89d3ac3a2da18cf1ce4554d99c574982e8e86968
Rapid-SBS <info@rapid-sbs.com>
2019-06-10 22:10:23 -0700

    Updates

--- skill-tree.php
+++ skill-tree.php
@@ -29,7 +29,7 @@
 
     <div class="row pad-10"><h4 class="mx-auto">LEVEL 10</h4></div>
 
-    <?php include 'skills/supports-tome.php'; ?>
+    <?php include 'skills/ultimates-ice.php'; ?>
 
   </div><!-- / Level 10 -->
 
--- skills/ultimates-ice.php
+++ skills/ultimates-ice.php
@@ -1,40 +1,39 @@
   <div class="row justify-content-center">
 
-    <!-- Sky Shatter -->  
-    <a class="btn text-white pad-10 mar-5 bg-purple" data-toggle="collapse" href="#collapsePlasmabeam" role="button" aria-expanded="false" aria-controls="collapsePlasmabeam">
-      <h6>Plasma Beam <span class="badge badge-light text-teal">9</span></h6>
+    <!-- Subzero Blade -->
+    <a class="btn text-white pad-10 mar-5 bg-purple" data-toggle="collapse" href="#collapseSubzeroblade" role="button" aria-expanded="false" aria-controls="collapseSubzeroblade">
+      <h6>Subzero Blade <span class="badge badge-light text-teal">10</span></h6>
     </a>
 
-    <!-- Absolute Zero -->
-    <a class="btn text-white pad-10 mar-5 bg-purple" data-toggle="collapse" href="#collapseMeteorstrike" role="button" aria-expanded="false" aria-controls="collapseMeteorstrike">
-      <h6>Meteor Strike <span class="badge badge-light text-teal">14</span></h6>
+    <!-- Frost Fury -->  
+    <a class="btn text-white pad-10 mar-5 bg-purple" data-toggle="collapse" href="#collapseFrostfury" role="button" aria-expanded="false" aria-controls="collapseFrostfury">
+      <h6>Frost Fury <span class="badge badge-light text-teal">9</span></h6>
     </a>
-
 
   </div><!-- /.row -->
 
   <div id="ultimate-ice" class="row mx-auto pad-10">
 
-    <!-- Plasma Beam -->
-    <div class="collapse mx-auto" id="collapsePlasmabeam" data-parent="#ultimate-ice">
+    <!-- Subzero Blade -->
+    <div class="collapse mx-auto" id="collapseSubzeroblade" data-parent="#ultimate-ice">
       <div class="card bg-light">
         <div class="card-header text-center">
           <h6>Ultimate Ability</h6>
         </div>
         <div class="card-body pad-5">
-          <p class="card-text text-center">Fire a beam so hot that it<br>pierces all enemy defenses.<br> Ignores all enemy RES. <br> DMG = 3 x (Player INT).<br> Costs 9 Mana.</p> 
+          <p class="card-text text-center">Shape a weapon of your choosing <br>to strike your foe.<br> Critical hits apply Frozen debuff instead of bonus damage.<br> DMG = 4 x (Player INT).<br> Costs 10 Mana.</p>
         </div>
       </div><!-- /.card -->
     </div><!-- /.collapse -->
 
-    <!-- Meteor Strike -->
-    <div class="collapse mx-auto" id="collapseMeteorstrike" data-parent="#ultimate-ice">
+    <!-- Frost Fury -->
+    <div class="collapse mx-auto" id="collapseFrostfury" data-parent="#ultimate-ice">
       <div class="card bg-light">
         <div class="card-header text-center">
           <h6>Ultimate Ability</h6>
         </div>
         <div class="card-body pad-5">
-          <p class="card-text text-center">Summon a meteor from the heavens<br>to crash into your enemies. <br> DMG = 4 x (Player INT).<br> Costs 14 Mana.</p>
+          <p class="card-text text-center">Create giant ice spikes high in the<br>atmosphere and rain them down on your foes.<br> DMG = 3 x (Player INT).<br> Costs 11 Mana.</p> 
         </div>
       </div><!-- /.card -->
     </div><!-- /.collapse -->
