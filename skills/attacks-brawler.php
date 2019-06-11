67335a9bcfce58ebd28d33c1e0e5e6443683c22a
Rapid-SBS <info@rapid-sbs.com>
2019-06-11 14:53:37 -0700

    Updates

---
+++ skills/attacks-brawler.php
@@ -0,0 +1,47 @@
+  <div class="row justify-content-center">
+
+    <!-- Crane Kick -->
+    <a class="btn text-white pad-10 mar-5 bg-blue" data-toggle="collapse" href="#collapseCranekick" role="button" aria-expanded="false" aria-controls="collapseCranekick">
+      <h6>Cranekick <span class="badge badge-light text-teal">6</span></h6>
+    </a>
+
+    <!-- Destabilize -->  
+    <a class="btn text-white pad-10 mar-5 bg-blue" data-toggle="collapse" href="#collapseDestabilize" role="button" aria-expanded="false" aria-controls="collapseDestabilize">
+      <h6>Destabilize <span class="badge badge-light text-teal">7</span></h6>
+    </a>
+
+  </div><!-- /.row -->
+
+  <div id="attack-brawler" class="row mx-auto pad-10">
+
+    <!-- Cranekick -->
+    <div class="collapse mx-auto" id="collapseCranekick" data-parent="#attack-brawler">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Attack Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Kick your opponent with perfect form.<br>DMG = 3 x (Player ATT).<br>Costs 5 Mana.</p>
+        </div>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+    <!-- Destabilize -->
+    <div class="collapse mx-auto" id="collapseDestabilize" data-parent="#attack-brawler">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Attack Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Attack the pressure points of your<br>foe, disrupting their chi flow.<br>Triple Critical Hit Chance.<br>Critical Hits apply Stun debuff.<br>DMG = 2 x (Player INT).<br> Costs 4 Mana.</p>
+        </div>
+        <ul class="list-group list-group-flush text-center">
+          <li class="list-group-item bg-light align-items-center" style="padding: .35rem 1.25rem;">
+            <span class="badge bg-white text-teal">3x</span>
+            Crit Chance
+          </li>
+        </ul>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+  </div><!-- /.row -->
---
+++ skills/supports-brawler.php
@@ -0,0 +1,47 @@
+  <div class="row justify-content-center">
+
+    <!-- Transcendence -->
+    <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseTranscendence" role="button" aria-expanded="false" aria-controls="collapseTranscendence">
+      <h6>Transcendence <span class="badge badge-light text-teal">6</span></h6>
+    </a>
+
+    <!-- Zen Aura -->  
+    <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseZenaura" role="button" aria-expanded="false" aria-controls="collapseZenaura">
+      <h6>Zen Aura <span class="badge badge-light text-teal">8</span></h6>
+    </a>
+
+  </div><!-- /.row -->
+
+  <div id="support-brawler" class="row mx-auto pad-10">
+
+    <!-- Transcendence -->
+    <div class="collapse mx-auto" id="collapseTranscendence" data-parent="#support-brawler">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Support Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Transcend the pain you've experienced,<br>restoring life to your body.<br>HEAL = 8 HP.<br> Costs 6 Mana.</p>
+        </div>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+    <!-- Zen Aura -->
+    <div class="collapse mx-auto" id="collapseZenaura" data-parent="#support-brawler">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Support Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Gift your allies a moment of pure<br>serenity, restoring their mana.<br> Costs 8 Mana.</p>
+        </div>
+        <ul class="list-group list-group-flush text-center">
+          <li class="list-group-item bg-light align-items-center" style="padding: .35rem 1.25rem;">
+            <span class="badge bg-white text-teal">+4</span>
+            Mana
+          </li>
+        </ul>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+  </div><!-- /.row -->
