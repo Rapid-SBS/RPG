dcb45c6a1f425b68b59d4e7445529268dc1ae246
Rapid-SBS <info@rapid-sbs.com>
2019-06-11 21:26:25 -0700

    Update supports-dagger.php

--- skills/supports-dagger.php
+++ skills/supports-dagger.php
@@ -1,39 +1,39 @@
   <div class="row justify-content-center">
 
-    <!-- Transcendence -->
-    <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseTranscendence" role="button" aria-expanded="false" aria-controls="collapseTranscendence">
-      <h6>Transcendence <span class="badge badge-light text-teal">6</span></h6>
+    <!-- Alchemic Potion -->
+    <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseAlchemicpotion" role="button" aria-expanded="false" aria-controls="collapseAlchemicpotion">
+      <h6>Alchemic Potion <span class="badge badge-light text-teal">6</span></h6>
     </a>
 
-    <!-- Toxic Bomb -->  
-    <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseToxicbomb" role="button" aria-expanded="false" aria-controls="collapseToxicbomb">
-      <h6>Toxic Bomb <span class="badge badge-light text-teal">5</span></h6>
+    <!-- Deadly Toxin -->  
+    <a class="btn text-white pad-10 mar-5 bg-green" data-toggle="collapse" href="#collapseDeadlytoxin" role="button" aria-expanded="false" aria-controls="collapseDeadlytoxin">
+      <h6>Deadly Toxin <span class="badge badge-light text-teal">5</span></h6>
     </a>
 
   </div><!-- /.row -->
 
   <div id="support-dagger" class="row mx-auto pad-10">
 
-    <!-- Transcendence -->
-    <div class="collapse mx-auto" id="collapseTranscendence" data-parent="#support-dagger">
+    <!-- Alchemic Potion -->
+    <div class="collapse mx-auto" id="collapseAlchemicpotion" data-parent="#support-dagger">
       <div class="card bg-light">
         <div class="card-header text-center">
           <h6>Support Ability</h6>
         </div>
         <div class="card-body pad-5">
-          <p class="card-text text-center">Transcend the pain you've experienced,<br>restoring life to your body.<br>HEAL = 8 HP.<br> Costs 6 Mana.</p>
+          <p class="card-text text-center">Consume your homebrewed potion<br>to regenerate health.<br>HEAL = 6 HP.<br> Costs 2 Mana.</p>
         </div>
       </div><!-- /.card -->
     </div><!-- /.collapse -->
 
-    <!-- Toxic Bomb -->
-    <div class="collapse mx-auto" id="collapseToxicbomb" data-parent="#support-dagger">
+    <!-- Deadly Toxin -->
+    <div class="collapse mx-auto" id="collapseDeadlytoxin" data-parent="#support-dagger">
       <div class="card bg-light">
         <div class="card-header text-center">
           <h6>Support Ability</h6>
         </div>
         <div class="card-body pad-5">
-          <p class="card-text text-center">Throw a bomb of noxious gas, applying<br>Deadly Poison debuff to all enemies.<br> Costs 5 Mana.</p>
+          <p class="card-text text-center">Disperse a specially-formulated toxin, applying<br>Deadly Poison debuff to all enemies.<br> Costs 5 Mana.</p>
         </div>
       </div><!-- /.card -->
     </div><!-- /.collapse -->
