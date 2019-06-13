e304c3697f204462a8f8c4fcf3915a8f6b3e3ad3
Rapid-SBS <info@rapid-sbs.com>
2019-06-12 22:18:46 -0700

    UPdate

---
+++ skills/attacks-windfu.php
@@ -0,0 +1,53 @@
+  <div class="row justify-content-center">
+
+    <!-- Air Slash -->
+    <a class="btn text-white pad-10 mar-5 bg-blue" data-toggle="collapse" href="#collapseAirslash" role="button" aria-expanded="false" aria-controls="collapseAirslash">
+      <h6>Air Slash <span class="badge badge-light text-teal">4</span></h6>
+    </a>
+
+    <!-- Shock Wave -->  
+    <a class="btn text-white pad-10 mar-5 bg-blue" data-toggle="collapse" href="#collapseShockwave" role="button" aria-expanded="false" aria-controls="collapseShockwave">
+      <h6>Shock Wave <span class="badge badge-light text-teal">6</span></h6>
+    </a>
+
+  </div><!-- /.row -->
+
+  <div id="attack-windfu" class="row mx-auto pad-10">
+
+    <!-- Air Slash -->
+    <div class="collapse mx-auto" id="collapseAirslash" data-parent="#attack-windfu">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Attack Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Generate a wind with incredible shear force.<br>Critical Hits apply Bleed debuff.<br>Double Critical Hit Chance.<br>DMG = Weapon ATT + Player INT.<br>Costs 4 Mana.</p>
+        </div>
+        <ul class="list-group list-group-flush text-center">
+          <li class="list-group-item bg-light align-items-center" style="padding: .35rem 1.25rem;">
+            <span class="badge bg-white text-teal">5</span>
+            Weapon ATT
+          </li>
+        </ul>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+    <!-- Shock Wave -->
+    <div class="collapse mx-auto" id="collapseShockwave" data-parent="#attack-windfu">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Attack Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Create a powerful shockwave that deals damage<br>to all enemies, possibly stunning them.<br>Double Critical Hit Chance.<br>Critical Hits apply Stun debuff.<br>DMG = 2 x (Player INT).<br> Costs 6 Mana.</p>
+        </div>
+        <ul class="list-group list-group-flush text-center">
+          <li class="list-group-item bg-light align-items-center" style="padding: .35rem 1.25rem;">
+            <span class="badge bg-white text-teal">2x</span>
+            Crit Chance
+          </li>
+        </ul>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+  </div><!-- /.row -->
---
+++ skills/ultimates-windfu.php
@@ -0,0 +1,52 @@
+  <div class="row justify-content-center">
+
+    <!-- Air Cannon -->  
+    <a class="btn text-white pad-10 mar-5 bg-purple" data-toggle="collapse" href="#collapseAircannon" role="button" aria-expanded="false" aria-controls="collapseAircannon">
+      <h6>Air Cannon <span class="badge badge-light text-teal">10</span></h6>
+    </a>
+
+    <!-- Vortex  -->
+    <a class="btn text-white pad-10 mar-5 bg-purple" data-toggle="collapse" href="#collapseHurricanestrike" role="button" aria-expanded="false" aria-controls="collapseHurricanestrike">
+      <h6>Hurricane Strike <span class="badge badge-light text-teal">13</span></h6>
+    </a>
+
+  </div><!-- /.row -->
+
+  <div id="ultimate-brawler" class="row mx-auto pad-10">
+
+    <!-- Air Cannon -->
+    <div class="collapse mx-auto" id="collapseAircannon" data-parent="#ultimate-brawler">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Ultimate Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Fire a blast of extreme air pressure<br>so intense it can shatter boulders.<br> DMG = 5 x (Player INT).<br> Costs 10 Mana.</p> 
+        <ul class="list-group list-group-flush text-center">
+          <li class="list-group-item bg-light align-items-center" style="padding: .35rem 1.25rem;">
+            <span class="badge bg-white text-orange">20</span>
+            Weapon ATT
+          </li>
+        </ul>
+        </div>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+    <!-- Hurricane Strike -->
+    <div class="collapse mx-auto" id="collapseHurricanestrike" data-parent="#ultimate-brawler">
+      <div class="card bg-light">
+        <div class="card-header text-center">
+          <h6>Ultimate Ability</h6>
+        </div>
+        <div class="card-body pad-5">
+          <p class="card-text text-center">Your Chi takes the form of a<br>dragon and consumes your enemies.<br>Ignores all enemy RES.<br> DMG = 3 x (Player INT).<br> Costs 12 Mana.</p>
+        </div>
+        <ul class="list-group list-group-flush text-center">
+          <li class="list-group-item bg-light align-items-center" style="padding: .35rem 1.25rem;">
+            Enemy RES = <span class="badge bg-white text-teal">0</span>
+          </li>
+        </ul>
+      </div><!-- /.card -->
+    </div><!-- /.collapse -->
+
+  </div><!-- /.row -->
