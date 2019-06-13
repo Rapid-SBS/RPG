3dfb8bdb4bf26738c163ea965107a467b82fe1f4
Rapid-SBS <info@rapid-sbs.com>
2019-06-13 14:07:12 -0700

    Update warrior-warhammer.php

--- weapons/warrior-warhammer.php
+++ weapons/warrior-warhammer.php
@@ -1,6 +1,6 @@
-  <!-- Warhammer-->
-  <div class="rpg-wpn-Warhammer card wide-275 mar-20 text-white bg-orange">
-  <div class="accordion" id="accordion-Warhammer">
+  <!-- War Hammer-->
+  <div class="rpg-wpn-warhammer card wide-275 mar-20 text-white bg-orange">
+  <div class="accordion" id="accordion-warhammer">
       <div class="card-header text-center">
         <h4>War Hammer</h4>
       </div>
@@ -9,9 +9,9 @@
           <h6>Slam</h6>
        </button>
       </div>
-      <div id="collapseThree" class="collapse hidden" aria-labelledby="headingThree" data-parent="#accordion-Warhammer">
+      <div id="collapseThree" class="collapse hidden" aria-labelledby="headingThree" data-parent="#accordion-warhammer">
         <div class="card-body light-transparency pad-10 text-center">
-          <p class="card-text text-center">Slam your Warhammer onto an enemy. <br>DMG = Weapon ATT + Player ATT. </p>
+          <p class="card-text text-center">Slam your War Hammer onto an enemy. <br>DMG = Weapon ATT + Player ATT. </p>
         </div>
         <ul class="list-group text-center">
           <li class="list-group-item align-items-center light-transparency bg-orange" style="padding: .35rem 1.25rem;">
@@ -25,9 +25,9 @@
           <h6>Swing <span class="badge badge-light text-teal">3</span></h6>
         </button>
       </div>
-      <div id="collapseFour" class="collapse hidden" aria-labelledby="headingFour" data-parent="#accordion-Warhammer">
+      <div id="collapseFour" class="collapse hidden" aria-labelledby="headingFour" data-parent="#accordion-warhammer">
         <div class="card-body light-transparency pad-10 text-center">
-          <p class="card-text text-center">Swing your warhammer and bowl over enemies.<br>DMG = Weapon ATT + Player ATT. <br>Costs 3 Mana.</p>
+          <p class="card-text text-center">Swing your War Hammer and bowl over enemies.<br>DMG = Weapon ATT + Player ATT. <br>Costs 3 Mana.</p>
         </div>
           <ul class="list-group text-center">
             <li class="list-group-item align-items-center light-transparency bg-orange" style="padding: .35rem 1.25rem;">
