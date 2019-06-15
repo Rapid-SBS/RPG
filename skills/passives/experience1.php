af534f33cb60d438fbae603d8de5b1ed21c64131
Rapid-SBS <info@rapid-sbs.com>
2019-06-15 12:07:53 -0700

    Update experience1.php

--- skills/passives/experience1.php
+++ skills/passives/experience1.php
@@ -1,16 +1,16 @@
   <div class="row justify-content-center">
 
     <!-- Experience I -->
-    <a class="btn text-white pad-10 mar-5 bg-purple" data-toggle="collapse" href="#collapseExperience1" role="button" aria-expanded="false" aria-controls="collapseExperience1">
-      <h6>Experience I <span class="badge badge-light text-teal">9</span></h6>
+    <a class="btn text-white pad-10 mar-5 bg-teal" data-toggle="collapse" href="#collapseExperience1" role="button" aria-expanded="false" aria-controls="collapseExperience1">
+      <h6>Experience I</h6>
     </a>
 
   </div><!-- /.row -->
 
-  <div id="ultimate-ice" class="row mx-auto pad-10">
+  <div id="passive-experience1" class="row mx-auto pad-10">
 
     <!-- Experience I -->
-    <div class="collapse mx-auto" id="collapseExperience1" data-parent="#ultimate-ice">
+    <div class="collapse mx-auto" id="collapseExperience1" data-parent="#passive-experience1">
       <div class="card bg-light">
         <div class="card-header text-center">
           <h6>Passive Skill</h6>
@@ -20,7 +20,7 @@
         </div>
         <ul class="list-group list-group-flush text-center">
           <li class="list-group-item bg-light align-items-center" style="padding: .35rem 1.25rem;">
-            <span class="badge bg-white text-purple">+1</span>
+            <span class="badge bg-white text-teal">+1</span>
             All Stats
           </li>
         </ul>
