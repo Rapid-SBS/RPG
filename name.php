917e04a46e5f065e32d16713d10bc1befd3c160f
Rapid-SBS <info@rapid-sbs.com>
2019-06-16 14:56:47 -0700

    Update name.php

--- name.php
+++ name.php
@@ -32,86 +32,7 @@
 </div><!-- /.row -->
 
 <div class="row">
-  <table class="wide-275 text-center table table-bordered mx-auto">
-    <tbody>
-      <tr>
-        <td>Name</td>
-        <td id="tb-rpg-name"> </td>
-      </tr>
-      <tr>
-        <td>Level</td>
-        <td id="tb-rpg-level"> </td>
-      </tr>
-      <tr>
-        <td>Class</td>
-        <td id="tb-rpg-class"> </td>
-      </tr>
-      <tr>
-        <td>Weapon</td>
-        <td id="tb-rpg-weapon"> </td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-
-<div class="row">
-  <h4 class="pad-10 mx-auto text-center" style>Combat Stats</h4>
-</div><!-- /.row -->
-<div class="row">
-  <table class="wide-275 text-center table table-bordered mx-auto">
-    <tbody>
-      <tr>
-        <td>HP</td>
-        <td id="tb-rpg-hp"> </td>
-      </tr>
-      <tr>
-        <td>ATT</td>
-        <td id="tb-rpg-att"> </td>
-      </tr>
-      <tr>
-        <td>DEF</td>
-        <td id="tb-rpg-def"> </td>
-      </tr>
-      <tr>
-        <td>INT</td>
-        <td id="tb-rpg-int"> </td>
-      </tr>
-      <tr>
-        <td>RES</td>
-        <td id="tb-rpg-res"> </td>
-      </tr>
-    </tbody>
-  </table>
-</div><!-- /.row -->
-
-<div class="row">
-  <h4 class="pad-10 mx-auto text-center" style>Abilities</h4>
-</div><!-- /.row -->
-<div class="row pad-b-20">
-  <table class="wide-275 text-center table table-bordered mx-auto">
-    <tbody>
-      <tr>
-        <td>Basic</td>
-        <td id="tb-rpg-basic" class="font-weight-bold text-red"> </td>
-      </tr>
-      <tr>
-        <td>Special</td>
-        <td id="tb-rpg-special" class="font-weight-bold text-yellow"> </td>
-      </tr>
-      <tr>
-        <td>Support</td>
-        <td id="tb-rpg-support" class="font-weight-bold text-green"> </td>
-      </tr>
-      <tr>
-        <td>Attack</td>
-        <td id="tb-rpg-attack" class="font-weight-bold text-blue"> </td>
-      </tr>
-      <tr>
-        <td>Ultimate</td>
-        <td id="tb-rpg-ultimate" class="font-weight-bold text-purple"> </td>
-      </tr>
-    </tbody>
-  </table>
+  <p class="text-center">You are a <span id="rpg-class"></span></p>
 </div><!-- /.row -->
 
 <script>
@@ -139,22 +60,7 @@ var weapon = JSON.parse(localStorage.getItem('objWeapon'));
   	player.attack = skills.attack;
   	player.ultimate = skills.ultimate;
 
-    $("#tb-rpg-name").text(player.name);
-    $("#tb-rpg-level").text(player.level);
-    $("#tb-rpg-class").text(player.class);
-    $("#tb-rpg-weapon").text(player.weapon);
-
-    $("#tb-rpg-hp").text(player.hp);
-    $("#tb-rpg-att").text(player.att);
-    $("#tb-rpg-def").text(player.def);
-    $("#tb-rpg-int").text(player.int);
-    $("#tb-rpg-res").text(player.res);
-
-    $("#tb-rpg-basic").text(player.basic);
-    $("#tb-rpg-special").text(player.special);
-    $("#tb-rpg-support").text(player.support);
-    $("#tb-rpg-attack").text(player.attack);
-    $("#tb-rpg-ultimate").text(player.ultimate);
+    $("#rpg-class").text("skilled " + rpgclass.name);
 
   });
 </script>
