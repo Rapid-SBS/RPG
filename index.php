111c661151c5fcea5038a88405f3267200357682
Rapid-SBS <info@rapid-sbs.com>
2019-06-17 10:06:16 -0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
   src="https://code.jquery.com/jquery-3.4.1.js"
   integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
   crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/vue"></script>
 
   <title>RPGenius</title>
 
@@ -275,21 +274,18 @@ var skills = {
   ultimate: undefined
 }
 
-localStorage.setItem('objPlayer', JSON.stringify(player));
-localStorage.setItem('objSkills', JSON.stringify(skills));
-
-$(document).ready(function(){
-
-
-  var app = new Vue({
+var app = new Vue({
     el: '#app',
     data: {
       product: 'Stuff',
       description: 'test'
     }
+})
 
-  })
+localStorage.setItem('objPlayer', JSON.stringify(player));
+localStorage.setItem('objSkills', JSON.stringify(skills));
 
+$(document).ready(function(){
 
   $("#rpg-class-warrior").click(function(){
     var rpgclass = {
@@ -378,7 +374,8 @@ $(document).ready(function(){
 
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vue"></script>
 </body>
 
 </html>
