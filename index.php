f9031891251951157997729e3f7b4fc7d176aca8
Rapid-SBS <info@rapid-sbs.com>
2019-06-17 12:06:10 -0700

    Update index.php

--- index.php
+++ index.php
@@ -239,22 +239,10 @@
   </a>
 
 </div><!-- /Class Selection -->
-<div id="app-player">
-  <h1 class="mx-auto">{{player.name}}</h1>
-</div>
 
 <script> 
-var player = new Vue({
-  el: '#app-player',
-  data: {
-    player: {
-      name: "Test Name"
-    }
-  }
-});
-
 // --- Initialize Player Object ---
-/* var player = {
+var player = {
   name: undefined,
   level: undefined,
   class: undefined,
@@ -269,7 +257,7 @@ var player = new Vue({
   support: undefined,
   attack: undefined,
   ultimate: undefined
-} */
+}
 
 var skills = {
   hp: 0,
@@ -282,7 +270,7 @@ var skills = {
   ultimate: undefined
 }
 
-/* localStorage.setItem('objPlayer', JSON.stringify(player)); */
+localStorage.setItem('objPlayer', JSON.stringify(player));
 localStorage.setItem('objSkills', JSON.stringify(skills));
 
 $(document).ready(function(){
@@ -371,7 +359,6 @@ $(document).ready(function(){
     <span class="text-muted">&copy;RPGenius 2019. All Rights Reserved.</span>
   </div>
 </footer>
-
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
