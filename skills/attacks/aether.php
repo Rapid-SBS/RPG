2ee13a883534b79eeca805a663b17bedc6378976
Rapid-SBS <info@rapid-sbs.com>
2019-06-19 12:39:18 -0700

    Updates

--- skills/attacks/aether.php
+++ skills/attacks/aether.php
@@ -11,7 +11,7 @@
         <input type="radio" name="picked" value="Light Shatter" v-model="attackSelection"><h6>Light Shatter <span class="badge badge-light text-teal">7</span></h6>
       </label>
       <br>
-      <h4 style="display: none;" id="attack-selection">{{attackSelection}}</h4>
+      <p id="attack-selection">{{attackSelection}}</p>
     </div>
   </div><!-- /.row -->
 
