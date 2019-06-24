5d3b191d75eb5a4f16e9f46fdaf60d9e5cdc4461
Rapid-SBS <info@rapid-sbs.com>
2019-06-24 10:31:12 -0700

    Update level.php

--- level.php
+++ level.php
@@ -34,13 +34,8 @@
 
 <div class="container max-wide-380">
 
-  <div class="row text-center mx-auto pad-t-40">
-    <div class="col">
-      <h5>Current Level: {{ value }} </h5>
-    </div>
-  </div><!-- /.row -->
-
 <div id="lvl" class="text-center mx-auto">
+  <h5>Current Level: {{ value }} </h5>
   <!-- default props -->
   <vue-slider
     ref="slider"
