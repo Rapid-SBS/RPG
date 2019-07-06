6e500766d071484be60d253e1d5beb9aa03345af
Rapid-SBS <info@rapid-sbs.com>
2019-07-05 21:30:52 -0700

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -1,10 +1,10 @@
 // --- Global Functions ---
-const getPlayerObj = () => { JSON.parse(localStorage.getItem('objPlayer')); };
-const getSkillsObj = () => { JSON.parse(localStorage.getItem('objSkills')); };
-const getRpgClassObj = () => { JSON.parse(localStorage.getItem('objRpgclass')); };
-const getWeaponObj = () => { JSON.parse(localStorage.getItem('objWeapon')); };
+const getPlayerObj = () => { JSON.parse(localStorage.getItem('objPlayer')); }
+const getSkillsObj = () => { JSON.parse(localStorage.getItem('objSkills')); }
+const getRpgClassObj = () => { JSON.parse(localStorage.getItem('objRpgclass')); }
+const getWeaponObj = () => { JSON.parse(localStorage.getItem('objWeapon')); }
 
-const setPlayerObj = () => { localStorage.setItem('objPlayer', JSON.stringify(player)); };
-const setSkillsObj = () => { localStorage.setItem('objSkills', JSON.stringify(skills)); };
-const setRpgClassObj = () => { localStorage.setItem('objRpgclass', JSON.stringify(rpgclass)); };
-const setWeaponObj = () => { localStorage.setItem('objWeapon', JSON.stringify(weapon)); };
+const setPlayerObj = () => { localStorage.setItem('objPlayer', JSON.stringify(player)); }
+const setSkillsObj = () => { localStorage.setItem('objSkills', JSON.stringify(skills)); }
+const setRpgClassObj = () => { localStorage.setItem('objRpgclass', JSON.stringify(rpgclass)); }
+const setWeaponObj = () => { localStorage.setItem('objWeapon', JSON.stringify(weapon)); }
