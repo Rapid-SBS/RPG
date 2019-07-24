fc7e80082a3b90bf07a8623b033f375281be5414
Rapid-SBS <info@rapid-sbs.com>
2019-07-24 15:42:39 -0700

    Update weapon-select.php

--- weapon-select.php
+++ weapon-select.php
@@ -1,4 +1,28 @@
-{{>header}}
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+
+  <script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
+
+  <title>RPGenerator</title>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="css/spacing.css">
+  <link rel="stylesheet" type="text/css" href="css/colors.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+<body>
 
 <h3 class="pad-t-20 text-gray text-center pulsing-text-dark"> Select a Weapon... </h3>
 
@@ -6,38 +30,43 @@
 
 <script>
 var rpgclass = JSON.parse(localStorage.getItem('objRpgclass'));
-var player = JSON.parse(localStorage.getItem('objPlayer'));
+
+var weapon = {
+  name: undefined,
+  basic: undefined,
+  special: undefined,
+}
 
   $(document).ready(function(){
 
     // ----- Loads the selected class -----
     if (rpgclass.name === "Warrior") {
 
-      $("#weapon-select").load("weapons/weapons-warrior.hbs");
+      $("#weapon-select").load("weapons/weapons-warrior.php");
       
     } else if (rpgclass.name === "Archer") {
 
-      $("#weapon-select").load("weapons/weapons-archer.hbs");
+      $("#weapon-select").load("weapons/weapons-archer.php");
 
     } else if (rpgclass.name === "Mage") {
 
-      $("#weapon-select").load("weapons/weapons-mage.hbs");
+      $("#weapon-select").load("weapons/weapons-mage.php");
       
     } else if (rpgclass.name === "Cleric") {
 
-      $("#weapon-select").load("weapons/weapons-cleric.hbs");
+      $("#weapon-select").load("weapons/weapons-cleric.php");
       
     } else if (rpgclass.name === "Rogue") {
 
-      $("#weapon-select").load("weapons/weapons-rogue.hbs");
+      $("#weapon-select").load("weapons/weapons-rogue.php");
       
     } else if (rpgclass.name === "Monk") {
 
-      $("#weapon-select").load("weapons/weapons-monk.hbs");
+      $("#weapon-select").load("weapons/weapons-monk.php");
       
     } else if (rpgclass.name === "Rune Master") {
 
-      $("#weapon-select").load("weapons/weapons-runemaster.hbs");
+      $("#weapon-select").load("weapons/weapons-runemaster.php");
       
     } else {
 
@@ -48,10 +77,17 @@ var player = JSON.parse(localStorage.getItem('objPlayer'));
 
 </div><!-- /.y-flex-container -->
 
-<div class="footer mt-auto text-center bg-lightgray">
-  <div class="container pad-t-20">
-    <a class="btn btn-dark text-white mar-r-10 mar-l-10" href="/" role="button">Restart</a>
+<footer class="footer mt-auto text-center bg-lightgray">
+  <div class="container pad-20">
+    <div class="pad-b-20">
+      <a class="btn btn-dark text-white mar-r-10 mar-l-10" href="http://rapid-phyre.net" role="button">Restart</a>
+    </div>
+    <span class="text-muted">&copy;RPGenius 2019. All Rights Reserved.</span>
   </div>
-</div>
-{{>footer}}
+</footer>
 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
+</body>
+
+</html>
